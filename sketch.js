7ecc2de0976d315cc32a2eac07698c9f38336c9d
k-trx <kathrynxia6971@gmail.com>
2024-01-25 10:45:45 -0500

    update wepage

---
+++ sketch.js
@@ -0,0 +1,36 @@
+let gif;
+
+function preload(){
+gif = loadImage('assets/KathrynGIF.gif');
+
+}
+
+
+function setup(){
+createCanvas(windowWidth, windowHeight);
+strokeWeight(1);
+
+
+}
+
+
+function draw(){
+background('cyan');
+var num = 10; 
+var sideLen = windowWidth/num;
+
+for (var y = 0; y < windowHeight; y+=sideLen){
+  
+  for(var x = 0; x< windowWidth; x+=sideLen){// loop to create a row of squares in the x direction
+ 
+ 
+ image(gif, x, y, sideLen, sideLen);
+  
+}
+}
+}
+
+function windowResized(){
+resizeCanvas(windowWidth, windowHeight)
+
+}
