eceeb6facc51c99117d479562eb78c0fc0cbca52
k-trx <kathrynxia6971@gmail.com>
2024-01-26 14:37:57 -0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -17,11 +17,11 @@ strokeWeight(1);
 function draw(){
 background('white');
 var num = 10; 
-var sideLen = windowWidth/num;
+var sideLen = windowWidth /num;
 
-for (var y = 0; y < windowHeight; y+=sideLen){
+for (var y = 0; y < windowHeight*2; y+=sideLen){
   
-  for(var x = 0; x< windowWidth; x+=sideLen){// loop to create a row of squares in the x direction
+  for(var x = 0; x< windowWidth*2; x+=sideLen){// loop to create a row of squares in the x direction
  
  
  image(gif, x, y, sideLen, sideLen);
